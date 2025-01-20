6974513d51fd47d0d9fdbdffd9ade944ea79460a
Tyler Totsch <utettotsch@gmail.com>
2025-01-19 21:19:35 -0800

    Added more functions for checking for duplicate files and showing the size of duplicate files

--- dr/dupe_remover.go
+++ dr/dupe_remover.go
@@ -6,13 +6,19 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	"golang.org/x/exp/slices"
 )
 
 func Find_dupes(folder_path string) []string {
 	files := get_files_from_dir(folder_path)
-	for _, file := range files {
-		fmt.Println(file)
-	}
+	// for _, file := range files {
+	// 	fmt.Println(file)
+	// }
+	dupes := check_for_dupes(files)
+	fmt.Println(dupes)
+	return_string := fmt.Sprintf("Size of Dupes: %s", int(get_size_of_files(dupes)))
+	fmt.Println(return_string)
 	return files
 }
 
@@ -40,4 +46,44 @@ func get_files_from_dir(folder_path string) []string {
 
 	}
 	return return_files
+}
+
+func check_for_dupes(files []string) []string {
+	same_files := make([]string, 0)
+
+	for _, file_name := range files {
+		file, err := os.Open(file_name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		current_file_info, err := file.Stat()
+		for _, check_file_name := range files {
+			check_file, err := os.Open(check_file_name)
+			if err != nil {
+				log.Fatal(err)
+			}
+			check_file_info, err := check_file.Stat()
+			if os.SameFile(current_file_info, check_file_info) && !slices.Contains(same_files, file_name) {
+				same_files = append(same_files, file_name)
+			}
+		}
+	}
+
+	return same_files
+}
+
+func get_size_of_files(files []string) int64 {
+	var files_size int64
+	for _, file_name := range files {
+		file, err := os.Open(file_name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		file_info, err := file.Stat()
+		files_size += file_info.Size()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return files_size
 }
